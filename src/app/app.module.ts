28193430e2b3f92451ce60f6f665e7c4db47eabe
Aldito8 <98069693+Aldito8@users.noreply.github.com>
2022-07-03 23:24:36 +0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './employee/dashboard/dashboard.component';
+import { ViewComponent } from './employee/view/view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ViewComponent,
+    NotFoundComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/edit-employee/edit-employee.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Employee } from 'src/app/employee';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-edit-employee',
+  templateUrl: './edit-employee.component.html',
+  styleUrls: ['./edit-employee.component.scss']
+})
+export class EditEmployeeComponent implements OnInit {
+  @Input() set employee(employee: Employee) {
+    if (employee) {
+      this.form.patchValue(employee);
+    }
+  }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public modal: NgbActiveModal
+  ) {
+    this.form = this.fb.group({
+      first: ['', Validators.required],
+      last: ['', Validators.required],
+      email: ['', Validators.required],
+      address: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  
+
+  submit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      this.modal.close(this.form.value);
+      Swal.fire({
+        text: 'data changed successfully',
+        target: '#custom-target',
+        customClass: {
+          container: 'position-absolute'
+        },
+        toast: true,
+        position: 'top-right'
+      })
+    }
+  }
+}
